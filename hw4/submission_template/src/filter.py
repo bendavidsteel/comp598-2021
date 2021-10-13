75050a3b1a895ccfd28afdec17c7b8a844b1cb4e
Ben Steel <bendavidsteel@gmail.com>
2021-10-13 15:02:19 -0400

    Updates to filter pipeline

--- hw4/submission_template/src/filter.py
+++ hw4/submission_template/src/filter.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 data_dir_path = os.path.join('/', 'home', 'ben', 'projects', 'comp598-2021', 'hw4', 'submission_template', 'data')
-dataset_path = os.path.join(data_dir_path, 'nyc_311_limit.csv')
+dataset_path = os.path.join(data_dir_path, 'nyc_311_limit_2020.csv')
 df = pd.read_csv(dataset_path, 
                  names=['Unique Key', 'Created Date', 'Closed Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor', 'Location Type', 'Incident Zip', 'Incident Address', 'Street Name', 'Cross Street 1', 'Cross Street 2', 'Intersection Street 1', 'Intersection Street 2', 'Address Type', 'City', 'Landmark', 'Facility Type', 'Status', 'Due Date', 'Resolution Description', 'Resolution Action Updated Date', 'Community Board', 'BBL', 'Borough', 'X Coordinate (State Plane)', 'Y Coordinate (State Plane)', 'Open Data Channel Type', 'Park Facility Name', 'Park Borough', 'Vehicle Type', 'Taxi Company Borough', 'Taxi Pick Up Location', 'Bridge Highway Name', 'Bridge Highway Direction', 'Road Ramp', 'Bridge Highway Segment', 'Latitude', 'Longitude', 'Location'],
                  dtype={'Incident Zip': 'str'})
---
+++ hw4/submission_template/src/filter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grep /2020 ./data/nyc_311_limit.csv > ./data/nyc_311_limit_2020.csv
