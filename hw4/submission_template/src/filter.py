b66ff9e845e1c2f818595af6c4553445f9fb3c1c
Ben Steel <bendavidsteel@gmail.com>
2021-10-13 16:24:16 -0400

    Preprocessing script

--- hw4/submission_template/src/filter.py
+++ hw4/submission_template/src/filter.py
@@ -2,19 +2,20 @@ import os
 import pandas as pd
 
 data_dir_path = os.path.join('/', 'home', 'ben', 'projects', 'comp598-2021', 'hw4', 'submission_template', 'data')
-dataset_path = os.path.join(data_dir_path, 'nyc_311_limit_2020.csv')
+dataset_path = os.path.join(data_dir_path, 'nyc_311_limit_slim.csv')
 df = pd.read_csv(dataset_path, 
-                 names=['Unique Key', 'Created Date', 'Closed Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor', 'Location Type', 'Incident Zip', 'Incident Address', 'Street Name', 'Cross Street 1', 'Cross Street 2', 'Intersection Street 1', 'Intersection Street 2', 'Address Type', 'City', 'Landmark', 'Facility Type', 'Status', 'Due Date', 'Resolution Description', 'Resolution Action Updated Date', 'Community Board', 'BBL', 'Borough', 'X Coordinate (State Plane)', 'Y Coordinate (State Plane)', 'Open Data Channel Type', 'Park Facility Name', 'Park Borough', 'Vehicle Type', 'Taxi Company Borough', 'Taxi Pick Up Location', 'Bridge Highway Name', 'Bridge Highway Direction', 'Road Ramp', 'Bridge Highway Segment', 'Latitude', 'Longitude', 'Location'],
                  dtype={'Incident Zip': 'str'})
 
 # filter cases that weren't open in 2020
-df = df[df['Created Date'].dt.year == 2020 | df['Closed Date'].dt.year == 2020]
+df = df[df['Created Date'].str.contains('/2020') | df['Closed Date'].str.contains('/2020')]
 
 # filter invalid zip codes
 df = df[df['Incident Zip'].notna()]
 
 # filter invalid dates
-df = df[df['Created Date'].dt < df['Closed Date'].dt]
+df['Created Date'] = pd.to_datetime(df['Created Date'], filter='%m/%d/%Y %I:%M:%S %p')
+df['Closed Date'] = pd.to_datetime(df['Closed Date'], filter='%m/%d/%Y %I:%M:%S %p')
+df = df[df['Created Date'] < df['Closed Date']]
 
 new_dataset_path = os.path.join(data_dir_path, 'nyc_311_limit_filtered.csv')
 df.to_csv(new_dataset_path)
---
+++ hw4/submission_template/src/process.py
@@ -0,0 +1,16 @@
+import os
+
+import pandas as pd
+
+this_dir_path = os.path.dirname(os.path.abspath(__file__))
+data_dir_path = os.path.join(this_dir_path, '..', 'data')
+dataset_path = os.path.join(data_dir_path, 'nyc_311_limit_filtered.csv')
+
+df = pd.read_csv(dataset_path, 
+                 dtype={'Incident Zip': 'str'})
+df['Created Date'] = pd.to_datetime(df['Created Date'], infer_datetime_format=True)
+df['Closed Date'] = pd.to_datetime(df['Closed Date'], infer_datetime_format=True)
+df['Duration'] = (df['Closed Date'] - df['Created Date']).dt.total_seconds // 3600
+df['Month'] = df['Closed Date'].dt.month
+
+
